6c5482a09c5f94d9608c9caa8f2cbdfc831a2d71
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2023-01-20 11:12:29 +0530

    parsed user details to template

--- org/views.py
+++ org/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from main.models import Donations,UserDetials
+from django.contrib import messages 
+from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 # Create your views here.
 @login_required
 def dashboard(request,template_name="org/dashboard.html"):
+    applied_for_donation_list = Donations.objects.filter(from_user__id=request.user.id)
+    user_details = UserDetials.objects.get(user__id=request.user.id)
     return render(request,template_name)
